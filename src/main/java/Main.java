d19e917a25342d22f2ea3c118725699642f68ca1
DMK1ng <93815275+DMK1ng@users.noreply.github.com>
2024-03-29 23:26:15 +0800

    Changes to be committed:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+//    System.out.println("Logs from your program will appear here!");
 
 //     Uncomment this block to pass the first stage
 
@@ -22,6 +22,28 @@ public class Main {
            head.createNewFile();
            Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
            System.out.println("Initialized git directory");
+         } catch (IOException e) {
+           throw new RuntimeException(e);
+         }
+       }
+
+       case "cat-file" -> {
+         String subCommandArg = args[1];
+         String hash = args[2];
+         if (hash.length() != 40) {
+           System.out.println("Hash wrong!");
+           return;
+         }
+
+         String dirHash = hash.substring(0, 2);
+         String fileHash = hash.substring(2);
+
+         File blobFile = new File("./.git/objects/" + dirHash + "/" + fileHash);
+
+         try {
+           String blob = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(blobFile)))).readLine();
+           String content = blob.substring(blob.indexOf('\0') + 1);
+           System.out.println(content);
          } catch (IOException e) {
            throw new RuntimeException(e);
          }
